393585e70247cc0315218b9b8af6d571630b5439
drgndenis <serkis-denis.dragan@ogr.iuc.edu.tr>
2024-02-10 20:52:06 +0300

    Initial Commit

---
+++ Movies/App/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Denis DRAGAN on 10.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
